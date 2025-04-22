450c0583549db1759aa9fbd8c9dec05ebb58375e
rafael ranzolin <147657601+rafaelranzolin@users.noreply.github.com>
2025-04-22 13:21:18 -0300

    .

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function calculaTempo(tempoObjetivo) {
     if (tempoFinal > 0) {
         return [dias, horas, minutos, segundos];
     } else {
-        return [6, 5, 49, 55];
+        return [12, 7, 49, 55];
     }
 }
 
